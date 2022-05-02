e43749f1047733507ba27249101360e7a86b5549
AlecUrbany <alecurbany@gmail.com>
2022-05-02 16:54:10 -0700

    Co-authored-by: NasserA <NasserA12433@users.noreply.github.com>

--- BFSTraversal.java
+++ BFSTraversal.java
@@ -1,4 +1,24 @@
 public class BFSTraversal
 {
-
+    Algorithm getBreadthFirstTraversal(originVertex)
+    traversalOrder = a new queue for the resulting traversal order
+    vertexQueue = a new queue to hold vertices as they are visited
+    Mark originVertex as visited
+    traversalOrder.enqueue(originVertex)
+    vertexQueue.enqueue(originVertex)
+    while (!vertexQueue.isEmpty())
+    {
+    frontVertex = vertexQueue.dequeue()
+    while (frontVertex has a neighbor)
+    {
+    nextNeighbor = next neighbor of frontVertex
+    if (nextNeighbor is not visited)
+    {
+    Mark nextNeighbor as visited
+    traversalOrder.enqueue(nextNeighbor)
+    vertexQueue.enqueue(nextNeighbor)
+    }
+    }
+    }
+    return traversalOrder
 }
--- DFSTraversal.java
+++ DFSTraversal.java
@@ -1,4 +1,24 @@
 public class DFSTraversal
 {
-    
+    Algorithm getDepthFirstTraversal(originVertex)
+    traversalOrder = a new queue for the resulting traversal order
+    vertexStack = a new stack to hold vertices as they are visited
+    Mark originVertex as visited
+    traversalOrder.enqueue(originVertex)
+    vertexStack.push(originVertex)
+    while (!vertexStack.isEmpty())
+    {
+    topVertex = vertexStack.peek()
+    if (topVertex has an unvisited neighbor)
+    {
+    nextNeighbor = next unvisited neighbor of topVertex
+    Mark nextNeighbor as visited
+    traversalOrder.enqueue(nextNeighbor)
+    vertexStack.push(nextNeighbor)
+    }
+    else // All neighbors are visited
+    vertexStack.pop()
+    }
+    return traversalOrder
+
 }
