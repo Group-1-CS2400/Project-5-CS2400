1464a19d7b8362904f393452251a657d83fd8c88
AlecUrbany <alecurbany@gmail.com>
2022-05-04 16:23:17 -0700

    Co-authored-by: NasserA <NasserA12433@users.noreply.github.com>

---
+++ Graph.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.HashMap;
+public class Graph implements graphInterface
+{
+    public void addVertex(String vertexLabel)
+    {
+     String vertex= new String ("A")
+    }
+    public void addEdge(String begin, String end) {
+       
+        if()
+        {
+             return true;
+        }
+         else
+         {
+             return false;
+         }
+    }
+    public void hasEdge(String begin, String end)
+    {
+        if()
+        {
+             return true;
+        }
+         else
+         {
+             return false;
+         }
+        
+    }
+    public char BFSTraversal()
+    {
+        /*
+        queue for function 
+        list for storage
+        list for reporting
+        */
+    }
+    public char DFSTraversal()
+    {
+         /*
+        queue for function 
+        list for storage
+        list for reporting
+        */
+
+    }
+   
+}
--- GraphInterface.java
+++ GraphInterface.java
@@ -1,30 +1,25 @@
-import java.util.List;
-import java.util.Queue;
-public interface GraphInterface<T> 
-{
-       /** Adds a given vertex to this graph.
+
+public interface graphInterface {
+  /** Adds a given vertex to this graph.
        @param vertexLabel  An object that labels the new vertex and is
                            distinct from the labels of current vertices.
        @return  True if the vertex is added, or false if not. */
-   public boolean addVertex(T vertexLabel);
-   /** Adds a weighted edge between two given distinct vertices that 
-       are currently in this graph. The desired edge must not already 
-       be in the graph. In a directed graph, the edge points toward
-       the second vertex given.
-       @param begin  An object that labels the origin vertex of the edge.
-       @param end    An object, distinct from begin, that labels the end
-                     vertex of the edge.
-       @param edgeWeight  The real value of the edge's weight.
-       @return  True if the edge is added, or false if not. */
-   public boolean addEdge(T begin, T end, double edgeWeight);
-
-   /** Adds an unweighted edge between two given distinct vertices 
-       that are currently in this graph. The desired edge must not
-       already be in the graph. In a directed graph, the edge points 
-       toward the second vertex given.
-       @param begin  An object that labels the origin vertex of the edge.
-       @param end    An object, distinct from begin, that labels the end
-                     vertex of the edge.
-       @return  True if the edge is added, or false if not. */
-   public boolean addEdge(T begin, T end);
+       public void addVertex(String vertexLabel);
+    
+       /** Adds an unweighted edge between two given distinct vertices 
+           that are currently in this graph. The desired edge must not
+           already be in the graph. In a directed graph, the edge points 
+           toward the second vertex given.
+           @param begin  An object that labels the origin vertex of the edge.
+           @param end    An object, distinct from begin, that labels the end
+                         vertex of the edge.
+           @return  True if the edge is added, or false if not. */
+       public void addEdge(String begin, String end);
+    
+       /** Sees whether an edge exists between two given vertices.
+           @param begin  An object that labels the origin vertex of the edge.
+           @param end    An object that labels the end vertex of the edge.
+           @return  True if an edge exists. */
+       public void hasEdge(String begin, String end);
+    
 }
