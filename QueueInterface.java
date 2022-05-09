40a5c9f366d0e68ec4dad867887305b9dd809b64
NasserA <98425768+NasserA12433@users.noreply.github.com>
2022-05-08 23:30:51 -0700

    Update QueueInterface.java

--- QueueInterface.java
+++ QueueInterface.java
@@ -4,21 +4,21 @@
    @author Timothy M. Henry
    @version 5.0
 */
-public interface QueueInterface<T>
+public interface QueueInterface<String>
 {
   /** Adds a new entry to the back of this queue.
       @param newEntry  An object to be added. */
-  public void enqueue(T newEntry);
+  public void enqueue(String newEntry);
   
   /** Removes and returns the entry at the front of this queue.
       @return  The object at the front of the queue. 
       @throws  EmptyQueueException if the queue is empty before the operation. */
-  public T dequeue();
+  public String dequeue();
   
   /**  Retrieves the entry at the front of this queue.
       @return  The object at the front of the queue.
       @throws  EmptyQueueException if the queue is empty. */
-  public T getFront();
+  public String getFront();
   
   /** Detects whether this queue is empty.
       @return  True if the queue is empty, or false otherwise. */
